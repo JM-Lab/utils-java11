87a59e266ff9ade76004ed32ed0e7d1811bb166e
Jemin Huh <hjm1980@gmail.com>
2022-01-26 17:23:45 +0900

    SimpleSIUnit 추가

---
+++ src/main/java/kr/jm/utils/enums/SimpleSIUnit.java
@@ -0,0 +1,114 @@
+package kr.jm.utils.enums;
+
+
+import kr.jm.utils.JMString;
+
+import java.text.DecimalFormat;
+import java.util.stream.Stream;
+
+/**
+ * The enum Simple si unit.
+ */
+public enum SimpleSIUnit {
+
+    /**
+     * K simple si unit.
+     */
+    K(1),
+    /**
+     * M simple si unit.
+     */
+    M(2),
+    /**
+     * G simple si unit.
+     */
+    G(3),
+    /**
+     * T simple si unit.
+     */
+    T(4),
+    /**
+     * P simple si unit.
+     */
+    P(5),
+    /**
+     * E simple si unit.
+     */
+    E(6);
+
+    /**
+     * The constant BASE_SIZE.
+     */
+    public static final int BASE_SIZE = 1024;
+    private double min;
+    private double max;
+
+    SimpleSIUnit(int power) {
+        this.min = Math.pow(BASE_SIZE, power);
+        this.max = this.min * BASE_SIZE;
+    }
+
+    /**
+     * Find si unit and convert to string string.
+     *
+     * @param size          the size
+     * @param decimalFormat the decimal format
+     * @param suffix        the suffix
+     * @return the string
+     */
+    public static String findSIUnitAndConvertToString(long size,
+            DecimalFormat decimalFormat, String suffix) {
+        return Stream.of(SimpleSIUnit.values())
+                .filter(si -> values()[0].min < size)
+                .filter(si -> size < si.max)
+                .map(si -> si.convertToString(size, decimalFormat, suffix))
+                .findFirst()
+                .orElseGet(() -> decimalFormat.format(size) + suffix);
+    }
+
+    /**
+     * Convert double.
+     *
+     * @param number the number
+     * @return the double
+     */
+    public double convert(Number number) {
+        return number.doubleValue() / min;
+    }
+
+    /**
+     * Append si unit string.
+     *
+     * @param target the target
+     * @param suffix the suffix
+     * @return the string
+     */
+    public String appendSIUnit(String target, String suffix) {
+        return target + JMString.SPACE + name() + suffix;
+    }
+
+    /**
+     * Convert to string string.
+     *
+     * @param size   the size
+     * @param suffix the suffix
+     * @return the string
+     */
+    public String convertToString(Number size, String suffix) {
+        return appendSIUnit(Double.toString(convert(size)), suffix);
+    }
+
+    /**
+     * Convert to string string.
+     *
+     * @param size          the size
+     * @param decimalFormat the decimal format
+     * @param suffix        the suffix
+     * @return the string
+     */
+    public String convertToString(Number size, DecimalFormat decimalFormat,
+            String suffix) {
+        return appendSIUnit(decimalFormat.format(convert(size)), suffix);
+    }
+
+}
